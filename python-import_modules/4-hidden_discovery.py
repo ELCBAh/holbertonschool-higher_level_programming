a01f77f32b790b746dd847b41c2c7357d1b8678c
ELCBAh <6211@holbertonstudents.com>
2023-06-04 21:04:06 +0000

    fixed loop not iterating through the loop properly

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 if __name__ == "__main__":
     funct_list = dir(hidden_4)
-    for i in funct_list:
+    for i in range(len(funct_list)):
         if funct_list[i][:2] != "__":
             print(funct_list[i])
