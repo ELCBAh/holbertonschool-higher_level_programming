986c5776b43cff98baa5903495726c98536e59bd
ELCBAh <6211@holbertonstudents.com>
2023-06-04 20:49:45 +0000

    trying to find out what the problem is with my code in the checker

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) == 3:
-        print("{}".format(int(sys.argv[1]) + int(sys.argv[2])))
+        print('{}'.format(int(sys.argv[1]) + int(sys.argv[2])))
