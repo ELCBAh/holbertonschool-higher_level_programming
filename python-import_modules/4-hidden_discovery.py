fa1b801b07cfb42ce4bb0074d0c7c37b055568cc
ELCBAh <6211@holbertonstudents.com>
2023-06-04 21:01:05 +0000

    fixed typo

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 if __name__ == "__main__":
     funct_list = dir(hidden_4)
     for i in funct_list:
-        if funct_list[i][0] != "_":
+        if funct_list[i][0] != "__":
             print(funct_list[i])
