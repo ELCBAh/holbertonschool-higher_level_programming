bebc2bb17ab3aa7bc9e0510b9af0083b8b5a7934
ELCBAh <6211@holbertonstudents.com>
2023-06-04 20:42:16 +0000

    pycodestyle fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,4 +2,4 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-    	print("{}".format(int(sys.argv[1]) + int(sys.argv[2])))
+        print("{}".format(int(sys.argv[1]) + int(sys.argv[2])))
