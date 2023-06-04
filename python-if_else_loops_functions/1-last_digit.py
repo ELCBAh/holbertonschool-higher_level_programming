31f2d2e298ef1fac4794b5e9fe30a2c1e96486de
ELCBAh <6211@holbertonstudents.com>
2023-06-04 17:34:02 +0000

    fixed pycodestyle error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,8 @@ else:
 if last == 0:
     print("Last digit of {0} is {1} and is 0".format(number, last))
 elif last > 5:
-    print("Last digit of {0} is {1} and is greater than 5".format(number, last))
+    prGr5 = "Last digit of {0} is {1} and is greater than 5"
+    print(prGr5.format(number, last))
 else:
-    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, last))
+    prLs6n0 = "Last digit of {0} is {1} and is less than 6 and not 0"
+    print(prLs6n0.format(number, last))
