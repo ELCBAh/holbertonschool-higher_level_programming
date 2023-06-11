c585442b215821802d0a8cdf0e31641fa3389efb
ELCBAh <6211@holbertonstudents.com>
2023-06-11 15:53:33 +0000

    excersise delete at updated, fixed checker

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return (new_list[idx] = None)
+    else:
+        del my_list[idx]
+    return my_list
