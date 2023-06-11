2d31dea1b3ec1fe1891e53d606e326a4d6773f5d
ELCBAh <6211@holbertonstudents.com>
2023-06-11 17:01:04 +0000

    fixed checker failing due to code not being adequate

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    new_matrix = []
     for i in range(len(matrix)):
+        new_matrix.append([])
         for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
+            new_matrix[i].append(matrix[i][j] * matrix[i][j])
     return new_matrix
