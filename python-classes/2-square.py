7f9c3a01f2ee707de8a769eba33d80c6738a8cd7
ELCBAh <6211@holbertonstudents.com>
2023-06-18 17:30:50 +0000

    added raise exception in certain conditions to square object

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square again but better"""
+
+
+class Square:
+    """Private instance attribute size init to None"""
+
+    def __init__(self, size=0):
+        """Init the size of square
+
+        Args:
+            size (int): size of square
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
