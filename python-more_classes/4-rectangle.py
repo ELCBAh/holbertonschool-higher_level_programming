1618b465a1fb5e67cc3b6fbd9b08874086aad63c
ELCBAh <6211@holbertonstudents.com>
2023-06-22 18:53:16 +0000

    fixed error caused by quotation marks

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -127,4 +127,4 @@ class Rectangle:
         return representation of rectangle using eval
 
         """
-        return "Rectangle(" + str(eval("self.width")) + ", " + str(eval("self.height")) + ")"
+        return 'Rectangle(' + str(eval('self.width')) + ', ' + str(eval('self.height')) + ')'
