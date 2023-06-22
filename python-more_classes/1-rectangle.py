448908f5e1bbb2382912d8e12931dd0918df3927
ELCBAh <6211@holbertonstudents.com>
2023-06-22 18:19:31 +0000

    defined height getter and setter with exception messages

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
 
     """
 
-    def __init__(self, width=0):
+    def __init__(self, width=0, height=0):
         """
 
         Initializing values and checking
@@ -23,7 +23,8 @@ class Rectangle:
         """
 
         self.__width = width
-    
+        self.__height = height:
+
     @property
     def width(self):
         """
@@ -48,3 +49,27 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+    @property
+    def height(self):
+        """
+        
+        getter for height
+
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+
+        setter for height
+
+        Args: value (int): height of the rectangle
+        
+        """
+        if value != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
