dcc6bc1e7097d9127d74281b8de838dd905c7832
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:09:04 +0000

    stopped working just because, trying to fix it

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -12,5 +12,6 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
