672b4fb787e52f09dc8cdb0d293950b707ce48cf
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:28:07 +0000

    changing str instance approach to fix checker errors

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -44,4 +44,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
 
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
