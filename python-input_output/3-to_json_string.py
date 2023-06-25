8d679d295fc8bc76920d39643a6be7d4ed05d25b
ELCBAh <6211@holbertonstudents.com>
2023-06-25 20:27:36 +0000

    to json string excercise added

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that returns JSON representation fo object string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    a function that returns JSON representation fo object string
+    """
+    import json
+    return json.dumps(my_obj)
