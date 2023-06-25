eb5daa395a5d6ead37e57ba1e84f13e79214db91
ELCBAh <6211@holbertonstudents.com>
2023-06-25 20:20:52 +0000

    fixed pycode style errors and added append write excercise

---
+++ python-inheritance/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that appends a string at the end of file
+
+returns number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends text at end of file
+    """
+    with open(filename, 'a+') as f:
+        return f.write(text)
