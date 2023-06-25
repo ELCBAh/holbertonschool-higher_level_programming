5560d1e4726fbc6c7582f7e1fb9220734e2560bb
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:26:16 +0000

    rolling back to previous commit importing module wont wokr

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+An empty class
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    def area(self):
+        """
+        An empty method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+
+        Args:
+            name (str): name
+            value (int): value to validate
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less than or equal to 0
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -12,6 +42,5 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
