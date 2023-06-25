4841dc36af4a523df1484a5081b0a0c4952e9ad1
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:45:32 +0000

    square excercise added

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+class square inheriting from rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+	"""
+	square class
+	"""
+	def __init__(self, size):
+		"""
+		initializing
+		"""
+		self.__size = size
+		self.integer_validator("size", size)
+		super().__init__(size, size)
+
+	def area(self):
+		"""
+		area
+		"""
+		return self.__size ** 2
