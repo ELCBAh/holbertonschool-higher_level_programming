4f19acb973ca3387bd0daf01299095f2219decd0
ELCBAh <6211@holbertonstudents.com>
2023-06-25 20:40:16 +0000

    removing sys and exception trying to fix checker

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,15 +2,10 @@
 """
 script that adds all arguments to a Py list and save in file
 """
-import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
-    try:
-        item = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        item = []
     item.extend(sys.argv[1:])
     save_to_json_file(item, "add_item.json")
