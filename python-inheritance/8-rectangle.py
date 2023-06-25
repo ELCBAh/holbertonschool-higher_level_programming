fc61815e5b343c7c6e9c08ccb5bcca9834a5a559
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:21:50 +0000

    having a hard time importing modules

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from 7-base_geometry import BaseGeometry
+from 7-base_geometry.py import BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
