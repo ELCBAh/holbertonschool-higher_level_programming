59bc63c152e7e9b027681a36e28998d919ca0091
ELCBAh <6211@holbertonstudents.com>
2023-06-25 20:08:51 +0000

    added README and fixed comments on read file excercise

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,11 @@ a function that reads a text file utf8 and prints to stdout
 
 
 def read_file(filename=""):
+    """
+    this function reads and prints to stdout
+
+    Parameters:
+        filename (str): the name of the file to read
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
