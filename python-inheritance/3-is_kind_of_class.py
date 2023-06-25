70eae0581794ddc55f3ffc61d7b80a9be0062ca5
ELCBAh <6211@holbertonstudents.com>
2023-06-25 18:33:24 +0000

    is kind of class excercise added

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function to check if object is an instance of inhereted class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function to check if object is an instance of inhereted class
+    """
+    return isinstance(obj, a_class)
