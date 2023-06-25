6411b8d37312d6c3d7f533cdec97e6b66974f867
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:33:51 +0000

    changing a few aspects to fix excercise not working for some uknown reason

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,35 +1,5 @@
 #!/usr/bin/python3
-"""
-An empty class
-"""
-
-
-class BaseGeometry():
-    """
-    An empty class
-    """
-    def area(self):
-        """
-        An empty method
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates value
-
-        Args:
-            name (str): name
-            value (int): value to validate
-        Raises:
-            TypeError: if value is not integer
-            ValueError: if value is less than or equal to 0
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
