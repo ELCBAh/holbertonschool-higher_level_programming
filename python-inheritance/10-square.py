710d54852b52bd0110a4855b203c451edd4b4168
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:46:16 +0000

    fixed pycodestyle errors

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,19 +6,19 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-	"""
-	square class
-	"""
-	def __init__(self, size):
-		"""
-		initializing
-		"""
-		self.__size = size
-		self.integer_validator("size", size)
-		super().__init__(size, size)
+    """
+    square class
+    """
+    def __init__(self, size):
+        """
+        initializing
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
 
-	def area(self):
-		"""
-		area
-		"""
-		return self.__size ** 2
+    def area(self):
+        """
+        area
+        """
+        return self.__size ** 2
