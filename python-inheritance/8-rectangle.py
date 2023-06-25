e482832951010bb2284b7d7ca2cb3591f59b3b7d
ELCBAh <6211@holbertonstudents.com>
2023-06-25 19:06:17 +0000

    changed call order to integer validator to fix checker fails

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle(BaseGeometry):
         """
         initializing
         """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
