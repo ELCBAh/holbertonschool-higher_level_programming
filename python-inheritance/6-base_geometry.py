ee3d5f1cbf7a443f700b32a5386b3542d8d8173a
ELCBAh <6211@holbertonstudents.com>
2023-06-25 18:39:50 +0000

    method area added to base geometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+An empty class
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    def area(self):
+        """
+        An empty method
+        """
+        raise Exception("area() is not implemented")
