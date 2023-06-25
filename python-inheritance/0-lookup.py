994a31c58045faad95e2bae3efcf28b944fb7964
ELCBAh <6211@holbertonstudents.com>
2023-06-25 18:08:16 +0000

    README file and excercise lookup added

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+A function that returns the list of available attributes and methods
+
+"""
+
+
+def lookup(obj):
+    """
+
+    Returns the list of available attributes and methods of an object
+
+    """
+
+    return dir(obj)
