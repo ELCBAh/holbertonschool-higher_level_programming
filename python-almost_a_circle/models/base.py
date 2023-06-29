01bacbcd5185ad14e019a6d8eb7540290d558d61
ELCBAh <6211@holbertonstudents.com>
2023-06-29 23:48:43 +0000

    updating code and addign tests foled

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,11 +7,13 @@ manages id attribute in all future classes
 avoid duplicating same code
 """
 
-class base:
+
+class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            base.__nb_objects += 1
-            self.id = base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
