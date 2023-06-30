62ee7d88746adc2bf8b711a64e53d48f17e31fa8
ELCBAh <6211@holbertonstudents.com>
2023-06-30 00:23:23 +0000

    now raising exceptions when valuerrors or typerrors to fix checker

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,6 +21,23 @@ class Rectangle(Base):
             y (int)
             id (int)
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         __width = width
         __height = height
         __x = x
