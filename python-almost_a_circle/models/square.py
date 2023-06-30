63d542ca6098778322712c00ef675244f0e661f6
ELCBAh <6211@holbertonstudents.com>
2023-06-30 17:54:45 +0000

    updated size setter to raise necessary exceptions

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,6 +20,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """size setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
 
     def __str__(self):
