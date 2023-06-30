fced507b7d655a7af1b0421ed46c1685f990622c
ELCBAh <6211@holbertonstudents.com>
2023-06-30 18:21:31 +0000

    implemented to jason string method and fixed weird error

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ manages id attribute in all future classes
 
 avoid duplicating same code
 """
+import json
 
 
 class Base:
@@ -18,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns json string representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
