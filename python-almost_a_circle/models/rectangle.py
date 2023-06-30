8a0ea147baaf6cf8e34a237fcda765a9b7cf1d05
ELCBAh <6211@holbertonstudents.com>
2023-06-30 00:36:46 +0000

    trying to fix checker

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,10 +38,10 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
 
-        __width = width
-        __height = height
-        __x = x
-        __y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -52,7 +52,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
-        pass
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -62,4 +66,8 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
-        pass
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if self.height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
