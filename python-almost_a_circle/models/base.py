0487f79eb5e570cef154659da37e6e946944a61e
ELCBAh <6211@holbertonstudents.com>
2023-06-30 18:24:29 +0000

    save to file method added

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,18 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns json string representation"""
+        """returns json string representation dictionary"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes json representation of list_objs to file"""
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dicts.append(obj.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(Base.to_json_string(list_dicts))
