0da02b914a5079282e4c0226b91e5932f3b94143
ELCBAh <6211@holbertonstudents.com>
2023-06-30 00:12:24 +0000

    removed extra content to meet precisely what is asked to do

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,11 +22,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+        pass
 
     @property
     def height(self):
@@ -34,8 +30,4 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+        pass
