cd76a114bfc7a4a193066f094b088d432310ec74
ELCBAh <6211@holbertonstudents.com>
2023-07-15 20:27:42 +0000

    added exec permissions to max integer test

--- holbertonschool-higher_level_programming/python-test_driven_development/tests/6-max_integer_test.py
+++ holbertonschool-higher_level_programming/python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """max_integer function unittests"""
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
