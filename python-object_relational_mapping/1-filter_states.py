9933e09b26a761b575b0acaea4d9e1af8b5cfe2b
ELCBAh <6211@holbertonstudents.com>
2023-07-30 23:30:25 +0000

    fixed pycodestyle failing and causing checker errors

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
     """
     Connecting to database and getting states
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states \
+                    WHERE name LIKE BINARY 'N%' \
+                    ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
