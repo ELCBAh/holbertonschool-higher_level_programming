fecc4eedf7d932250c4fc678dec3f0f2742a99b4
ELCBAh <6211@holbertonstudents.com>
2023-07-30 21:46:14 +0000

    fixing even more syntax errors

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities' (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     'state_id' INT NOT NULL,
     'name' VARCHAR(256) NOT NULL,
-    FOREIGN KEY('state_id') REFERENCES 'hbtn_0d_usa'.'states'('id'),
+    FOREIGN KEY('state_id') REFERENCES hbtn_0d_usa.states(id),
 );
