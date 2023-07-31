e043aaa87cc44bc98942a98d9e0f757e091c1137
ELCBAh <6211@holbertonstudents.com>
2023-07-31 03:34:30 +0000

    given exec permissions, fixed pycodestyle

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    Connecting to database and getting cities
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Louisiana')
+    session.add(state)
+    session.commit()
+    print(state.id)
