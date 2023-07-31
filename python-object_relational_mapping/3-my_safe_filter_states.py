15cf6a8c50b72394e2edc8e2aea383427d0dda7c
ELCBAh <6211@holbertonstudents.com>
2023-07-31 02:04:57 +0000

    fixing checker errors

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,11 +13,9 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states \
-                    WHERE name LIKE BINARY %(name) \
-                    ORDER BY states.id ASC", {'name': sys.argv[4]})
+                    WHERE name=%s", (sys.argv[4], ))
     rows = cursor.fetchall()
-    if rows is not None:
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
