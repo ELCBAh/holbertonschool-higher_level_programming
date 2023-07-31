93624d91acab0dbb5764f61b3386f55733aec7d0
ELCBAh <6211@holbertonstudents.com>
2023-07-31 03:06:15 +0000

    fixed pycodestyle style error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     """
     Connecting to database and getting cities
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
