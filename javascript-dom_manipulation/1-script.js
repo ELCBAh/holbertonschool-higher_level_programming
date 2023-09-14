a99e238ec643ebe673d7fa6ed6d3204455dfbfd6
ELCBAh <6211@holbertonstudents.com>
2023-09-14 15:13:07 +0000

    fixed missing semicolon

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -4,4 +4,4 @@ header.style.color = '#FF0000';
 const red_header = document.querySelector('#red_header');
 red_header.addEventListener('click', function(){
     header.style.color = '#FF0000';
-})
+});
