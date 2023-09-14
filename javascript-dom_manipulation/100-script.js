d6998a992117ebfd6db71b317d727d0c367171d0
ELCBAh <6211@holbertonstudents.com>
2023-09-14 18:26:51 +0000

    after reading documentation finally came to the solution. Fixed due to a syntax error

--- javascript-dom_manipulation/100-script.js
+++ javascript-dom_manipulation/100-script.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('add_item').addEventListener('click', function() {
-        var list = document.querySelector('#my_list');
+        var list = document.querySelector('.my_list');
         var new_li = document.createElement('li');
         new_li.innerText = 'Item';
         list.appendChild(new_li);
     });
 
     document.getElementById('remove_item').addEventListener('click', function() {
-        var list = document.querySelector('my_list');
+        var list = document.querySelector('.my_list');
         var last_li = list.lastElementChild;
         list.removeChild(last_li);
     });
 
     document.getElementById('clear_list').addEventListener('click', function() {
-        var list = document.querySelector('my_list');
+        var list = document.querySelector('.my_list');
         while (list.firstChild) {
             list.removeChild(list.firstChild);
         }
