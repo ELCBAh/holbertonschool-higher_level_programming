31f2463b8b1111f4de304afdb09f1cb529f11362
ELCBAh <6211@holbertonstudents.com>
2023-09-14 17:39:16 +0000

    adding 3_script excercise and fixed syntax error

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('toggle_header').addEventListener('click', function() {
+        var header = document.querySelector('header');
+
+        if (header.classList.contains('red')) {
+            header.classList.remove('red');
+            header.classList.add('green');
+        } else if (header.classList.contains('green')) {
+            header.classList.remove('green');
+            header.classList.add('red');
+        } else {
+            header.classList.add('red');
+        }
+    });
+});
