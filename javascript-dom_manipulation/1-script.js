e95178e730856f3f3203dcc221a4aba87bfb0bf8
ELCBAh <6211@holbertonstudents.com>
2023-09-14 15:17:34 +0000

    fixed requirements misinterpretation, should work now.

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,6 +1,3 @@
-const header = document.querySelector('header');
-header.style.color = '#FF0000';
-
 const red_header = document.querySelector('#red_header');
 red_header.addEventListener('click', function(){
     header.style.color = '#FF0000';
