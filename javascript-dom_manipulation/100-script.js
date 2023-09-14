05dae84945d34b69433b92686c8c7d5d114c856a
ELCBAh <6211@holbertonstudents.com>
2023-09-14 18:10:49 +0000

    100-script exercise added

---
+++ javascript-dom_manipulation/100-script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('add_item').addEventListener('click', function() {
+        var list = document.getElementById('my_list');
+        var new_li = document.createElement('li');
+        new_li.innerText = 'Item';
+        list.appendChild(new_li);
+    });
+
+    document.getElementById('remove_item').addEventListener('click', function() {
+        var list = document.getElementById('my_list');
+        var last_li = list.lastElementChild;
+        list.removeChild(last_li);
+    });
+
+    document.getElementById('clear_list').addEventListener('click', function() {
+        var list = document.getElementById('my_list');
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    });
+});
