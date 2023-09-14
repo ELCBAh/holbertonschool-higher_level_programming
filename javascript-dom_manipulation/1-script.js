c71f4882152f7e8de03cc79d8d12c82f07ad1b31
ELCBAh <6211@holbertonstudents.com>
2023-09-14 15:23:03 +0000

    wrapped code in event listener to run after DOM to fix script not working

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,8 @@
-const red_header = document.querySelector('#red_header');
-red_header.addEventListener('click', function(){
-    header.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function(){
+    const red_header = document.querySelector('#red_header');
+    const header = document.querySelector('header');
+
+    red_header.addEventListener('click', function(){
+        header.style.color = '#FF0000';
+    });
 });
