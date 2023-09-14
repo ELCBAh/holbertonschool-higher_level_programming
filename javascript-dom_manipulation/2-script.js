1ba713b184f1f48fc138bc71c3296c3fecaa9af5
ELCBAh <6211@holbertonstudents.com>
2023-09-14 15:36:27 +0000

    2_script excercise added, changing only one line from previous excercise was necessary

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const red_header = document.querySelector('#red_header');
+    const header = document.querySelector('header');
+
+    red_header.addEventListener('click', function(){
+        headerElement.classList.add('red');
+    });
+});
