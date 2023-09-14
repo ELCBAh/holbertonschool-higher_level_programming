97e98441d4661cf39c9d75afbfae21277d93660f
ELCBAh <6211@holbertonstudents.com>
2023-09-14 15:11:22 +0000

    script excercise added

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
+
+const red_header = document.querySelector('#red_header');
+red_header.addEventListener('click', function(){
+    header.style.color = '#FF0000';
+})
