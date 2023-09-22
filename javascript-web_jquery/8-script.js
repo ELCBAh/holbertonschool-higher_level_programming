ead84598ef6c8894e93c1e4322e8bec3b77247da
ELCBAh <6211@holbertonstudents.com>
2023-09-22 23:48:04 +0000

    8-script added

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
+  for (const movie of data.results) {
+    $('ul#list_movies').append('<li>' + movie.title + '</li>');
+  }
+});
